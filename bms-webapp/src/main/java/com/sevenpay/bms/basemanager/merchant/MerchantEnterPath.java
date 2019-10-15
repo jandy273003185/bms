58195cb6467ea7b69bd2a760a0e52cba63b7542d
shili <shili@szgyzb.com>
2019-10-15 10:05:56 +0800

    商户录入path

---
+++ bms-webapp/src/main/java/com/sevenpay/bms/basemanager/merchant/MerchantEnterPath.java
@@ -0,0 +1,33 @@
+package com.sevenpay.bms.basemanager.merchant;
+
+public final class MerchantEnterPath {
+	
+	/** 商户录入*/
+	public final static String BASE = "/merchant/enter";
+
+	/**
+	 * 列表
+	 */
+	public final static String LIST ="/list";
+	
+	/**
+	 * 添加
+	 */
+	public final static String ADD ="/add";
+	/**
+	 * 预览
+	 */
+	public final static String PREVIEW = "/preview";
+	/**
+	 * 更新
+	 */
+	public final static String UPDATE = "/update";
+	/**
+	 * 审核
+	 */
+	public final static String AUDIT ="/audit";
+	/**
+	 * 产品查看
+	 */
+	public final static String PRODUCT = "/product";
+}
