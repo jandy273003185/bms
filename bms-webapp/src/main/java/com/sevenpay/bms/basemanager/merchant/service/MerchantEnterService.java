7547bb558b410cd5d74021c751cd1818e883dafc
唐聪 <tangcongzxc@foxmail.com>
2019-10-17 18:51:03 +0800

    修改商户新增功能

---
+++ bms-webapp/src/main/java/com/sevenpay/bms/basemanager/merchant/service/MerchantEnterService.java
@@ -0,0 +1,92 @@
+package com.sevenpay.bms.basemanager.merchant.service;
+
+import com.sevenpay.bms.basemanager.acctsevenbuss.mapper.AcctSevenBussMapper;
+import com.sevenpay.bms.basemanager.bank.service.BankService;
+import com.sevenpay.bms.basemanager.custInfo.mapper.TdCustInfoMapper;
+import com.sevenpay.bms.basemanager.merchant.bean.MerchantVo;
+import com.sevenpay.bms.basemanager.merchant.dao.MerchantDao;
+import com.sevenpay.bms.basemanager.merchant.mapper.*;
+import com.sevenpay.bms.expresspay.CommonService;
+import com.sevenpay.invoke.SevenpayCoreServiceInterface;
+import org.activiti.engine.IdentityService;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.apache.commons.codec.binary.Base64;
+import org.sevenpay.platform.web.myWorkSpace.service.WorkSpaceService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * 商户注册服务类v2
+ */
+@Service
+public class MerchantEnterService {
+    private static Base64 base64 = new Base64();
+
+    @Autowired
+    private CommonService commonService;
+    @Autowired
+    private BankService bankService;
+    @Autowired
+    SevenpayCoreServiceInterface sevenpayCoreServiceInterface;
+    @Autowired
+    private TdLoginUserInfoMapper mapper;
+
+    @Autowired
+    private MerchantMapper merchantMapper;
+
+    @Autowired
+    private CustScanMapper custScanMapper;
+
+    @Autowired
+    private MerchantDao dao;
+
+    @Resource
+    private RepositoryService repositoryService;
+
+    @Resource
+    private RuntimeService runtimeService;
+
+    @Resource
+    private TaskService taskService;
+
+    @Resource
+    private IdentityService identityService;
+
+    @Autowired
+    private BmsProtocolContentMapper bmsProtocolContentMapper;
+
+    @Autowired
+    private TdCustInfoMapper tdCustInfoMapper;
+
+    @Autowired
+    private WorkSpaceService workSpaceService;
+
+    @Autowired
+    private TdLoginUserInfoMapper tdLoginUserInfoMapper;
+
+    @Autowired
+    private AcctSevenBussMapper acctSevenBussMapper;
+
+    @Autowired
+    private StoreManageMapper storeManageMapper;
+
+    private static final Logger logger = LoggerFactory.getLogger(MerchantService.class);
+
+    /**
+     * 注册通过后的商户信息v2
+     *
+     * @param merchantVo
+     * @return
+     */
+    public List<MerchantVo> selectAuditMerchants(MerchantVo merchantVo) {
+        List<MerchantVo> list = dao.auditList2(merchantVo);
+        return list;
+    }
+}
