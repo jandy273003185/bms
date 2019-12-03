f81275c4d87c4964a76f1b0d7dc5d9d76b094305
jintong <614069071@qq.com>
2019-12-03 11:09:22 +0800

    增加修改弹框组件

--- bms-ui/src/main.js
+++ bms-ui/src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import ElementUI from 'element-ui'
 
 import SiteIcon from './components/SiteIcon'
+import AlertModel from './components/AlertModel'
 
 import './css/base.scss';
 import './css/common.scss';
@@ -12,7 +13,9 @@ import './css/mixins.scss';
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(ElementUI);
+
 Vue.component('site-icon', SiteIcon);
+Vue.component('alert-model', AlertModel);
 
 Vue.config.productionTip = false
 
