c2c668f039e0e10b8c527e3457ddfdf4c54f217e
zhangguangchao <zhangguangchao@192.168.1.108>
2019-12-04 11:49:59 +0800

    分账

---
+++ bms-webapp/src/main/java/com/qifenqian/bms/merchant/subAccount/bean/MerchantSubAccouontBean.java
@@ -0,0 +1,150 @@
+package com.qifenqian.bms.merchant.subAccount.bean;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 商户关系表
+ * @author Lx
+ *
+ */
+public class MerchantSubAccouontBean implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2926040873839050957L;
+
+	/** ID */
+	private String id;
+	/** 客户号 */
+	private String custId;
+	/** 外部商户号 */
+	private String outMerchantCode;
+	/** 分账方类型 */
+	private String subAccountType;
+	/** 分账方账号 */
+	private String account;
+	/** 分账方全称 */
+	private String accountName;
+	/** 与分账方的关系类型 */
+	private String relationType;
+	/** 微信自定义关系 */
+	private String customRelation;
+	/** 外部请求号 */
+	private String outRequestNo;
+	/** 分账比例 */
+	private String rate;
+	/** 备注 */
+	private String remark;
+	/** 创建时间 */
+	private Date createTime;
+	/** 修改时间 */
+	private Date modifyTime;
+	/**渠道*/
+	private String channelCode;
+	/**请求添加分账方状态*/
+	private String reportStatus;
+	/**使用状态*/
+	private String status;
+	
+	
+	public String getChannelCode() {
+		return channelCode;
+	}
+	public void setChannelCode(String channelCode) {
+		this.channelCode = channelCode;
+	}
+	public String getReportStatus() {
+		return reportStatus;
+	}
+	public void setReportStatus(String reportStatus) {
+		this.reportStatus = reportStatus;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	public Date getModifyTime() {
+		return modifyTime;
+	}
+	public void setModifyTime(Date modifyTime) {
+		this.modifyTime = modifyTime;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getCustId() {
+		return custId;
+	}
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
+	public String getOutMerchantCode() {
+		return outMerchantCode;
+	}
+	public void setOutMerchantCode(String outMerchantCode) {
+		this.outMerchantCode = outMerchantCode;
+	}
+	public String getSubAccountType() {
+		return subAccountType;
+	}
+	public void setSubAccountType(String subAccountType) {
+		this.subAccountType = subAccountType;
+	}
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	public String getAccountName() {
+		return accountName;
+	}
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+	public String getRelationType() {
+		return relationType;
+	}
+	public void setRelationType(String relationType) {
+		this.relationType = relationType;
+	}
+	public String getCustomRelation() {
+		return customRelation;
+	}
+	public void setCustomRelation(String customRelation) {
+		this.customRelation = customRelation;
+	}
+	public String getOutRequestNo() {
+		return outRequestNo;
+	}
+	public void setOutRequestNo(String outRequestNo) {
+		this.outRequestNo = outRequestNo;
+	}
+	public String getRate() {
+		return rate;
+	}
+	public void setRate(String rate) {
+		this.rate = rate;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	
+	
+}
