cdf2da42f2aeba511b6315cc07f586a4fbc2d862
jintong <614069071@qq.com>
2019-12-06 17:51:48 +0800

    角色管理页面优化

--- bms-ui/src/views/System/Role/Role.vue
+++ bms-ui/src/views/System/Role/Role.vue
@@ -21,15 +21,15 @@
 
       <template slot="tableInner">
         <el-table :data="tableData" border style="width: 100%">
-          <el-table-column prop="serial" min-width="50" label="编号"></el-table-column>
-          <el-table-column prop='usercode' min-width="100" label="角色代码"></el-table-column>
-          <el-table-column prop='username' min-width="100" label="角色名称"></el-table-column>
-          <el-table-column prop='regenerator' min-width="100" label="角色说明"></el-table-column>
-          <el-table-column prop='valid' min-width="80" label="是否有效"></el-table-column>
-          <el-table-column prop='creator' min-width="80" label="创建人"></el-table-column>
-          <el-table-column prop='date' min-width="200" label="创建时间"></el-table-column>
-          <el-table-column prop='lastupdate' min-width="100" label="最后更改人"></el-table-column>
-          <el-table-column prop='turnovertime' min-width="200" label="最后更改时间"></el-table-column>
+          <el-table-column prop="name1" min-width="50" label="编号"></el-table-column>
+          <el-table-column prop='name2' min-width="100" label="角色代码"></el-table-column>
+          <el-table-column prop='name3' min-width="100" label="角色名称"></el-table-column>
+          <el-table-column prop='name4' min-width="100" label="角色说明"></el-table-column>
+          <el-table-column prop='name5' min-width="80" label="是否有效"></el-table-column>
+          <el-table-column prop='name6' min-width="80" label="创建人"></el-table-column>
+          <el-table-column prop='name7' min-width="200" label="创建时间"></el-table-column>
+          <el-table-column prop='name8' min-width="100" label="最后更改人"></el-table-column>
+          <el-table-column prop='name9' min-width="200" label="最后更改时间"></el-table-column>
           <el-table-column fixed="right" label="操作" width="140">
             <template slot-scope="scope">
               <el-button type="text" size="small" @click="lookClick(scope.row)">查看</el-button>
@@ -46,7 +46,7 @@
     </page-model>
 
     <!-- 角色新增 -->
-    <alert-model v-show="addDisplay" title="角色新增" @on-submit="addModelSubmit" @on-cancel="addModelCancel">
+    <el-dialog title="角色新增" :visible.sync="addDisplay" width="600px">
       <el-form ref="alertAddModelForm" :model="addModelData" class="alert-model-form" label-width="80px" :show-message="false">
         <el-form-item prop="name1" label="角色名称" required>
           <el-input v-model="addModelData.name1"></el-input>
@@ -69,11 +69,15 @@
           </el-checkbox-group>
         </el-form-item>
       </el-form>
-    </alert-model>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="addModelCancel">取 消</el-button>
+        <el-button type="primary" @click="addModelSubmit">确 定</el-button>
+      </div>
+    </el-dialog>
 
     <!-- 修改 查看 model -->
-    <alert-model v-show="editorDisplay" title="角色修改" @on-submit="editorModelSubmit" @on-cancel="editorModelCancel" :hideBtn="hideBtn">
-      <el-form :disabled="look_editor" ref="alertEditorModelForm" :model="editorModelData" class="alert-model-form" label-width="80px" :show-message="false">
+    <el-dialog title="角色修改" :visible.sync="editorDisplay" width="600px">
+      <el-form ref="alertEditorModelForm" :model="editorModelData" class="alert-model-form" label-width="80px" :show-message="false">
         <el-form-item prop="name1" label="角色名称" required>
           <el-input v-model="editorModelData.name1"></el-input>
         </el-form-item>
@@ -95,21 +99,25 @@
           </el-checkbox-group>
         </el-form-item>
       </el-form>
-    </alert-model>
+      <div v-show="islook" slot="footer" class="dialog-footer">
+        <el-button @click="editorModelCancel">取 消</el-button>
+        <el-button type="primary" @click="editorModelSubmit">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 const testData = {
-  serial: '1',
-  usercode: 'clear',
-  username: '王小虎',
-  regenerator: '所有权限',
-  valid: '有效',
-  creator: '创建人',
-  date: '2015-12-22 6:08:54',
-  lastupdate: '石理',
-  turnovertime: '2019-12-04 03:53:03'
+  name1: '1',
+  name2: 'clear',
+  name3: '王小虎',
+  name4: '所有权限',
+  name5: '有效',
+  name6: '创建人',
+  name7: '2015-12-22 6:08:54',
+  name8: '石理',
+  name9: '2019-12-04 03:53:03'
 };
 
 // 活动性质选项
@@ -155,8 +163,7 @@ export default {
         //修改model数据
         name5: modelDataCheckboxList
       },
-      look_editor: false, //编辑还是查看
-      hideBtn: false, //查看时，隐藏按钮组
+      islook: false, //编辑还是查看
       tableData: new Array(5).fill(testData),
       paginationOps: {
         pageSizes: [5, 10, 15, 20],
@@ -172,33 +179,32 @@ export default {
   },
   created() {},
   methods: {
-    addModelSubmit(c) {
+    addModelSubmit() {
       console.log(this.addModelData);
-      c();
+      this.addDisplay = false;
     },
     addModelCancel() {
       this.addDisplay = false;
       // this.resetFormFields('alertAddModelForm');
     },
-    editorModelSubmit(c) {
+    editorModelSubmit() {
+      // http提交数据
       console.log(this.editorModelData);
-      c();
+      this.editorDisplay = false;
     },
     editorModelCancel() {
       this.editorDisplay = false;
-      this.look_editor = false;
-      this.hideBtn = false;
     },
     editorClick(row) {
       this.editorDisplay = true;
-      this.look_editor = false;
+      this.islook = true;
+      this.editorModelData = row;
       console.log(row);
     },
     lookClick(row) {
       this.editorDisplay = true;
-      this.look_editor = true;
-      this.hideBtn = true;
-      // this.editorModelData = row;
+      this.islook = false;
+      this.editorModelData = row;
       console.log(row);
     },
     goToSearch() {
