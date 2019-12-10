bbffa6a8f3d0166c13ba9ee6da9344a880e230b3
jintong <614069071@qq.com>
2019-12-10 10:59:28 +0800

    新增财务管理=>财务查询=>商户余额

--- bms-ui/src/views/Financial/Query/Index.vue
+++ bms-ui/src/views/Financial/Query/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="financial-query-page">
     <el-tabs v-model="tabsIndex" type="card" @tab-click="handleClick">
-      <el-tab-pane v-for="(item,index) in tabs" :key="index" :label="item.label" :name="`${item.cpt}`"></el-tab-pane>
+      <el-tab-pane v-for="(item,index) in tabs" :key="index" :label="item.label" :name="item.cpt"></el-tab-pane>
     </el-tabs>
     <components :is="tabsIndex" />
   </div>
@@ -11,14 +11,15 @@
 import { sstorage } from '@/tools/utils';
 import Total from './Total';
 import Variation from './Variation';
+import Commercial from './Commercial';
 export default {
-  components: { Total, Variation },
+  components: { Total, Variation, Commercial },
   data() {
     return {
       tabs: [
         { label: '汇总余额', cpt: 'Total' },
         { label: '余额变动', cpt: 'Variation' },
-        { label: '商户余额', cpt: 'Total3' },
+        { label: '商户余额', cpt: 'Commercial' },
         { label: '用户余额', cpt: 'Total4' },
         { label: '客戶提现结算', cpt: 'Total5' },
         { label: '商户提现结算', cpt: 'Total6' },
